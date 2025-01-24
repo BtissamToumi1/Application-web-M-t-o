7b67da9955ba81687b46e216d46b8710a6fe9aff
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-24 11:25:01 +0100

    l,ajout de la fonction localisation

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ search.addEventListener("click",()=>{
 
 });
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=15bd6169f4518f74e064ad787e0c48dc`;
-const urlall = `https://api.openweathermap.org/data/2.5/forecast?q=london&units=metric&appid=15bd6169f4518f74e064ad787e0c48dc`;
- afficher(url,urlall);
+
 function afficher(url,urlall){
 
 fetch(url)
@@ -33,22 +31,32 @@ fetch(url)
 fetch(urlall)
 .then(Response => Response.json())
 .then( data => {
+    // for (let i = 0; i < 6; i++) {
+    //     const tmp1 = document.getElementById("temp_day1");
+    // tmp1.innerHTML=data.list[i].main.temp+"<sup>°c</sup>";
+    // const des1= document.getElementById("description-1");
+    // des1.innerHTML=data.list[i].weather[i].description;
+    // const humidity1 ="<sup> Hymidity: </sup>"+document.getElementById("hymidity_1");
+    // humidity1.innerHTML=data.list[i].main.humidity+"<span>%</span>";
+    // const wind1 ="<sup> Wind speed: </sup>"+document.getElementById("wind_1");
+    // wind1.innerHTML=data.list[i].wind.speed; 
+    //   }
+
     //day 1
     console.log(data);
     const tmp1 = document.getElementById("temp_day1");
     tmp1.innerHTML=data.list[0].main.temp+"<sup>°c</sup>";
     const des1= document.getElementById("description-1");
     des1.innerHTML=data.list[0].weather[0].description;
-    const humidity1 ="<sup> Hymidity: </sup>"+document.getElementById("hymidity_1");
-    humidity1.innerHTML=data.list[0].main.humidity+"<span>%</span>";
-    const wind1 ="<sup> Wind speed: </sup>"+document.getElementById("wind_1");
-    wind1.innerHTML=data.list[0].wind.speed; 
-    //day 2
+     const humidity1 ="<sup> Hymidity: </sup>"+document.getElementById("hymidity_1");
+     humidity1.innerHTML=data.list[0].main.humidity+"<span>%</span>";
+     const wind1 ="<sup> Wind speed: </sup>"+document.getElementById("wind_1");
+     wind1.innerHTML=data.list[0].wind.speed;     //day 2
     const tmp2 = document.getElementById("temp_day2");
     tmp2.innerHTML=data.list[6].main.temp+"<sup>°c</sup>";
     const des2= document.getElementById("description-2");
     des2.innerHTML=data.list[6].weather[0].description;
-   const humidity2 ="<sup> Hymidity: </sup>"+ document.getElementById("hymidity_2");
+    const humidity2 ="<sup> Hymidity: </sup>"+ document.getElementById("hymidity_2");
     humidity2.innerHTML=data.list[6].main.humidity+"<span>%</span>";
     const wind2 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_2");
     wind2.innerHTML=data.list[6].wind.speed; 
@@ -70,7 +78,7 @@ fetch(urlall)
     des4.innerHTML=data.list[22].weather[0].description;
     const humidity4 ="<sup> Hymidity: </sup>"+ document.getElementById("hymidity_4");
     humidity4.innerHTML=data.list[22].main.humidity+"<span>%</span>";
-    const wind4 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_4");
+    const wind4 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_4")+"<sup> Wind speed: </sup>";
     wind4.innerHTML=data.list[22].wind.speed;
 
       //day 5
@@ -79,22 +87,40 @@ fetch(urlall)
       const des5= document.getElementById("description-4");
       des5.innerHTML=data.list[30].weather[0].description;
       const humidity5 ="<sup> Hymidity: </sup>"+ document.getElementById("hymidity_5");
-      humidity5.innerHTML=data.list[30].main.humidity+"<span>%</span>";
-      const wind5 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_5");
-      wind5.innerHTML=data.list[30].wind.speed;
+       humidity5.innerHTML=data.list[30].main.humidity+"<span>%</span>";
+       const wind5 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_5")+"<sup> Wind speed: </sup>";
+       wind5.innerHTML=data.list[30].wind.speed;
 
-      //day 6
-      const tmp6 = document.getElementById("temp_day6");
+//       //day 6
+     const tmp6 = document.getElementById("temp_day6");
       tmp6.innerHTML=data.list[38].main.temp+"<sup>°c</sup>";
-      const des6= document.getElementById("description-6");
-      des6.innerHTML=data.list[38].weather[0].description;
+     const des6= document.getElementById("description-6");
+       des6.innerHTML=data.list[38].weather[0].description;
       const humidity6 ="<sup> Hymidity: </sup>"+ document.getElementById("hymidity_6");
       humidity6.innerHTML=data.list[38].main.humidity+"<span>%</span>";
-      const wind6 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_6");
-      wind6.innerHTML=data.list[38].wind.speed;
+      const wind6 ="<sup> Wind speed: </sup>"+ document.getElementById("wind_6")+"<sup> Wind speed: </sup>";
+       wind6.innerHTML=data.list[38].wind.speed;
 
       city.value="";
 
 });
-
 }
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position) {
+    let x,y;
+    let url1,urlall1
+    x= position.coords.latitude;
+    y= position.coords.longitude;
+    url1="https://api.openweathermap.org/data/2.5/weather?lat="+x+"&lon="+y+"&appid=15bd6169f4518f74e064ad787e0c48dc&units=metric";
+    urlall1="https://api.openweathermap.org/data/2.5/forecast?lat="+x+"&lon="+y+"&appid=15bd6169f4518f74e064ad787e0c48dc&units=metric";
+    console.log(url1);
+    afficher(url1,urlall1);
+  }
+
